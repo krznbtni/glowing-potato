ae1aa90b7a4a414b3a6137992c1b94d3f9f3fb61
Karzan Botani <karzan@botani.nu>
2022-06-08 05:19:24 +0200

    refactor: removed test script

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-function test() {
-  curl -X POST http://localhost:3000/client/"$1"
-  curl -X POST -H 'Content-Type: application/json' -d '{ "result": "ok" }' http://localhost:3000/webhook/"$1"
-}
-
-test 1
