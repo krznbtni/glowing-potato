3eb2d29cda736ca89578909aa4cefb97909a3903
Karzan Botani <karzan@botani.nu>
2022-06-08 05:18:34 +0200

    feat: use client and webhook route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { createServer } from 'http';
 import express from 'express';
 
+import { api } from './api/index.js';
+
 const app = express();
 const server = createServer(app);
 const port = 3000;
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use(api);
+
 server.listen(port, () => console.log(`Server listening on port ${port}`));
