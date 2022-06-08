03b1c40dc2c60ecdd3eeaac19282c26ad4c9b329
Karzan Botani <karzan@botani.nu>
2022-06-08 05:53:35 +0200

    refactor: cleaner service call. More informative parameter names

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,13 @@
 const results = {};
 
 export async function callService (clientId) {
-  results[clientId] = {};
-  return results[clientId].promise = new Promise((r) => {
-    results[clientId].resolve = r;
+  return new Promise((resolve) => {
+    results[clientId] = resolve;
   });
 }
 
-export function resolveResult (clientId, r) {
-  results[clientId].resolve(r);
+export function resolveResult (clientId, body) {
+  results[clientId](body);
   delete results[clientId];
 }
 
